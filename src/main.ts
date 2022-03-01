bf0b8cf0730ea404362345d3a6c2b3f99e0cffcd
JinHyuck Cha <jhcha@superb-ai.com>
2022-03-01 21:50:14 +0900

    Init db mocking and Add faker package

--- src/main.ts
+++ src/main.ts
@@ -20,6 +20,7 @@ async function bootstrap() {
   app.useGlobalPipes(
     new ValidationPipe({
       // DTO에 정의 되지 않은 값이 들어오면 request 자체를 막음
+      // @IsString()과 같이 decorator를 무조건 달아야함
       forbidNonWhitelisted: true,
       // @없는 속성 들어오면 해당 속성 제거
       whitelist: true,
--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,6 +1,18 @@
+import { faker } from '@faker-js/faker';
 import { Test, TestingModule } from '@nestjs/testing';
+import { Repository } from 'typeorm';
+import { CreateUserDto } from './dto/create-user.dto';
 import { UserRepository } from './repositories/user.repository';
 import { UsersService } from './users.service';
+
+// mocking db
+type MockRepository<T = any> = Partial<Record<keyof Repository<T>, jest.Mock>>;
+const createMockRepository = <T = any>(): MockRepository<T> => ({
+  findOne: jest.fn(),
+  create: jest.fn(),
+});
+// for time out
+jest.setTimeout(10000);
 
 describe('UsersService', () => {
   let service: UsersService;
@@ -15,7 +27,10 @@ describe('UsersService', () => {
     userRepository = module.get<UserRepository>(UserRepository);
   });
 
-  // describe('createUser', () => {
-  //   it('should create a user', async () => {});
-  // });
+  describe('createUser', () => {
+    it('should create a user', async () => {
+      // const firstName = faker.name.firstName();
+      // const lastName = faker.name.lastName();
+    });
+  });
 });
